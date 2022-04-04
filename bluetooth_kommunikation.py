aad4b192cf2a52756022506c2b9a65efdc99f786
Robin Justinger <robinjustinger@gmail.com>
2022-04-04 21:24:42 +0200

    Bluetooth-Funktionalität in eine Klasse gebaut Version ist nicht schön, funktioniert aber fürs Erste

--- bluetooth_kommunikation.py
+++ bluetooth_kommunikation.py
@@ -10,6 +10,43 @@ from kivy.uix.label import Label
 from bleak import BleakScanner, BleakClient
 from bleak.exc import BleakError
 
+MAKEBLOCKDevice = 0
+
+class Bluetooth_connection:
+    def __init__(self):
+        self.MAKEBLOCKDevice = None
+        self.send_request = False
+        self.direction = None
+        self.read_characteristic = f"0000{0xFFF4:x}-0000-1000-8000-00805f9b34fb"
+        self.write_characteristic = f"0000{0xFFF1:x}-0000-1000-8000-00805f9b34fb"
+
+    async def bluetooth_verbinden(self):
+        devices = await BleakScanner.discover()
+        for d in devices:
+            if d.name == 'MAKEBLOCK':
+                MAKEBLOCKDevice = d
+                print("MAKEBLOCK wurde gefunden!")
+                async with BleakClient(MAKEBLOCKDevice) as client:
+                    print("Direkt vor connect")
+                    client.connect(timeout=10.0)
+                    if not client.is_connected:
+                        print("Fehler, die Bluetooth-Verbindung kann nicht hergestellt werden!")
+                    else:
+                        print("Bluetooth-Verbindung erfolgreich hergestellt!")
+
+                    await client.start_notify(f"0000{0xFFF4:x}-0000-1000-8000-00805f9b34fb", notification_handler)
+                    #try:
+                    while True:
+                        if self.send_request:
+                            await client.write_gatt_char(f"0000{0xFFF1:x}-0000-1000-8000-00805f9b34fb", self.direction)
+                            self.send_request = False
+                        await asyncio.sleep(1.0)
+
+                    #except:
+                        #print("Die Verbindung ist aus unbekannten Gründen abgebrochen!")
+                    await client.stop_notify(f"0000{0xFFF4:x}-0000-1000-8000-00805f9b34fb")
+
+
 def notification_handler(sender, data):
     """Simple notification handler which prints the data received."""
     output_numbers = list(data)
@@ -23,25 +60,60 @@ def eventloop(function):
 
 async def bluetooth_verbinden():
     devices = await BleakScanner.discover()
-    MAKEBLOCKDevice = 0
+    #MAKEBLOCKDevice = 0
     for d in devices:
         if d.name == 'MAKEBLOCK':
             MAKEBLOCKDevice = d
+            print("MAKEBLOCK wurde gefunden!")
+            async with BleakClient(MAKEBLOCKDevice) as client:
+                print("Direkt vor connect")
+                client.connect(timeout=10.0)
+                if not client.is_connected:
+                    print("Fehler, die Bluetooth-Verbindung kann nicht hergestellt werden!")
+                else:
+                    print("Bluetooth-Verbindung erfolgreich hergestellt!")
 
+                await client.start_notify(f"0000{0xFFF4:x}-0000-1000-8000-00805f9b34fb", notification_handler)
+                try:
+                    while True:
+                        if send_request:
+                            await client.write_gatt_char(f"0000{0xFFF1:x}-0000-1000-8000-00805f9b34fb", b'Hello')
+                        await asyncio.sleep(5.0)
+
+                except:
+                    print("Die Verbindung ist aus unbekannten Gründen abgebrochen!")
+                await client.stop_notify(f"0000{0xFFF4:x}-0000-1000-8000-00805f9b34fb")
+                #await client.start_notify(f"0000{0xFFF1:x}-0000-1000-8000-00805f9b34fb", notification_handler)
+                #await asyncio.sleep(5.0)
+                #await client.stop_notify(f"0000{0xFFF1:x}-0000-1000-8000-00805f9b34fb")
+
+async def richtung_fahren(Richtung):
+    async with BleakClient(MAKEBLOCKDevice) as client:
+        await client.stop_notify(f"0000{0xFFF4:x}-0000-1000-8000-00805f9b34fb")
+        await client.write_gatt_char(f"0000{0xFFF1:x}-0000-1000-8000-00805f9b34fb", bytes(5))
+'''
     if MAKEBLOCKDevice == 0:
         print("Fehler, kein MAKEBLOCK gefunden!")
     else:
+        print("MAKEBLOCK wurde gefunden!")
         async with BleakClient(MAKEBLOCKDevice) as client:
             await client.connect()
             if not client.is_connected:
                 print("Fehler, die Bluetooth-Verbindung kann nicht hergestellt werden!")
+            else:
+                print("Bluetooth-Verbindung erfolgreich hergestellt!")
 
             await client.start_notify(f"0000{0xFFF4:x}-0000-1000-8000-00805f9b34fb", notification_handler)
             # await client.write_gatt_char(f"0000{0xFFF1:x}-0000-1000-8000-00805f9b34fb", bytes(5))
             await asyncio.sleep(10.0)
             await client.stop_notify(f"0000{0xFFF4:x}-0000-1000-8000-00805f9b34fb")
 
+'''
+'''
+def anweisung_senden(direction):
+    await client.write_gatt_char(f"0000{0xFFF1:x}-0000-1000-8000-00805f9b34fb", bytes(5))
 
+'''
 #scan for available bluetooth-devices and connect to the device called "MAKEBLOCK"
 async def bluetooth():
     devices = await BleakScanner.discover()
--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import asyncio
 import bleak
 import platform
 import sys
+import time
 import bluetooth_kommunikation
 from bluetooth_kommunikation import eventloop
+from bluetooth_kommunikation import bluetooth_verbinden
+from bluetooth_kommunikation import richtung_fahren
+from bluetooth_kommunikation import Bluetooth_connection
 kivy.require('1.11.1')
 from kivy.app import App
 from kivy.uix.label import Label
@@ -17,28 +21,51 @@ from kivy.uix.floatlayout import FloatLayout
 from bleak import BleakScanner, BleakClient
 from bleak.exc import BleakError
 from kivy.properties import ObjectProperty
+from threading import Thread
+
+Makeblock_connection = Bluetooth_connection()
+
+def start_coroutine(routine):
+    Loop = asyncio.new_event_loop()
+    Thread(target=Loop.run_forever, daemon=True).start()
+    Loop.call_soon_threadsafe(asyncio.create_task, routine)
 
 class Raumerfassung(Widget):
     automatikstatusanzeige = ObjectProperty(None)
 
     def fahre_vor(self):
         print("Fahre vor!")
+        Makeblock_connection.direction = b'f'
+        Makeblock_connection.send_request = True
+
 
     def fahre_zurueck(self):
         print("Fahre zurück!")
+        Makeblock_connection.direction = b'b'
+        Makeblock_connection.send_request = True
+
 
     def fahre_links(self):
         print("Fahre links!")
+        Makeblock_connection.direction = b'l'
+        Makeblock_connection.send_request = True
+
 
     def fahre_rechts(self):
         print("Fahre rechts!")
+        Makeblock_connection.direction = b'r'
+        Makeblock_connection.send_request = True
+
 
     def verbinde_bluetooth(self):
-        print("Bluetooth wird verbunden!")
-        eventloop(bluetooth_kommunikation.bluetooth_verbinden())
+        start_coroutine(Makeblock_connection.bluetooth_verbinden())
+        time.sleep(1)
 
     def automatik_einschalten(self):
         print("Automatik wird wieder eingeschaltet!")
+        Makeblock_connection.direction = b'a'
+        Makeblock_connection.send_request = True
+
 
 
 class RaumerfassungApp(App):
@@ -78,4 +105,4 @@ Servic = asyncio.run(Test.get_services())
 for service in Servic:
     print(service)
 """
-#asyncio.run(bluetooth_communication.bluetooth())
+#asyncio.run(bluetooth_kommunikation.bluetooth_verbinden())
